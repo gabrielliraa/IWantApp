3566c61584bc86afa1617d19f488a7e4811a5068
gabriel.lira <gabriel.lira@radixeng.com.br>
2023-01-30 14:53:17 -0300

    Add project files.

---
+++ Domain/Products/Category.cs
@@ -0,0 +1,6 @@
+﻿namespace IWantApp.Domain.Products;
+
+public class Category : Entity
+{
+
+}
---
+++ Domain/Products/Product.cs
@@ -0,0 +1,12 @@
+﻿namespace IWantApp.Domain.Products;
+
+public class Product
+{
+
+    public string Description { get; set; }
+
+    public string Category { get; set; }
+
+    public bool HasStok { get; set; }
+
+}
