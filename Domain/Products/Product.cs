e65ac86cdf8a05d4e45e5b26782786e8409ad87d
gabriel.lira <gabriel.lira@radixeng.com.br>
2023-01-31 16:29:50 -0300

    Contexto do EF - Criando o DbContext

--- Domain/Products/Product.cs
+++ Domain/Products/Product.cs
@@ -1,11 +1,13 @@
 ﻿namespace IWantApp.Domain.Products;
 
-public class Product
+public class Product : Entity
 {
 
     public string Description { get; set; }
 
     public string Category { get; set; }
+
+    public int CategoryId { get; set; }
 
     public bool HasStok { get; set; }
 
