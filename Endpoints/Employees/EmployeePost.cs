c0e13b3d9ce5f96dd8fa81c3bd07f05ef7ac186b
gabriel.lira <gabriel.lira@radixeng.com.br>
2023-02-10 14:13:45 -0300

    salvando usuario final e adicionando Claims

--- Endpoints/Employees/EmployeePost.cs
+++ Endpoints/Employees/EmployeePost.cs
@@ -1,6 +1,7 @@
 ﻿using IWantApp.Domain.Products;
 using IWantApp.Infra.Data;
 using Microsoft.AspNetCore.Identity;
+using System.Security.Claims;
 
 namespace IWantApp.Endpoints.Employees;
 
@@ -17,6 +18,19 @@ public class EmployeePost
         var result = userManager.CreateAsync(user, employeeRequest.Password).Result;
 
         if(!result.Succeeded)
+        {
+            return Results.BadRequest(result.Errors.First());
+        }
+
+
+        var userClaims = new List<Claim>
+        {
+            new Claim("EmployeeCode", employeeRequest.EmployeeCode),
+            new Claim("Name", employeeRequest.Name)
+        };
+        var claimResult = userManager.AddClaimsAsync(user, userClaims).Result;
+
+        if (!claimResult.Succeeded)
         {
             return Results.BadRequest(result.Errors.First());
         }
